abca414ad2477a3873ed97939ce1820fbd62ee44
luckh-coder <luckhero@gmail.com>
2025-06-03 21:00:22 +0200

    update

--- giftwebsite/giftwebsite/settings.py
+++ giftwebsite/giftwebsite/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from datetime import timedelta
 SECRET_KEY = 'django-insecure-hn&(7(_g3n#5y84^4c+j$5xtj5*&wbj11x+9t!-4_f%%uew2*@'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -89,12 +89,25 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'bbEuDkmHNIXBaCrEFdThyeQpNMehnbAe',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
+
 AUTHENTICATION_BACKENDS = [
     'giftapp.backends.EmailBackend',
     'django.contrib.auth.backends.ModelBackend',
